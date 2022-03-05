37a9052c6a4a0767d216e075676971e748b4ce75
Jean Ruiz <ruizlaruin@gmail.com>
2022-03-05 13:46:36 -0500

    Actualizxacion del menu y correcciones en las ultimas dos opciones.

--- src/main/java/controladores/AdministradorProductos.java
+++ src/main/java/controladores/AdministradorProductos.java
@@ -21,8 +21,13 @@ public class AdministradorProductos implements ServicioProductos {
 
     @Override
     public void productosPorDefecto(){
-        this.productosAlmacenados.add(new Productos("BIC28371", "Bicicleta", "CJR1837", 500.0));
-        this.productosAlmacenados.add(new Productos("EDP23938", "Equipos de proteccion", "AT18272", 80.0));
-        this.productosAlmacenados.add(new Productos("REP39481", "Repuestos", "CJR1837", 75.0));
+        this.productosAlmacenados.add(new Productos("BIC28371", "Bicicleta Gama Baja", "CJR1837", 200.0));
+        this.productosAlmacenados.add(new Productos("BIC28371", "Bicicleta Gama Media", "CJR1837", 350.0));
+        this.productosAlmacenados.add(new Productos("BIC28371", "Bicicleta Profesional", "CJR1837", 500.0));
+        this.productosAlmacenados.add(new Productos("EDP23938", "Equipos de proteccion", "AT18272", 40.0));
+        this.productosAlmacenados.add(new Productos("REP39481", "Casco Entrenamiento", "CJR1837", 75.0));
+        this.productosAlmacenados.add(new Productos("REP39481", "Casco Profesional", "CJR1837", 200.0));
+        this.productosAlmacenados.add(new Productos("REP39481", "Repuestos - Neumatico", "CJR1837", 5.0));
+        this.productosAlmacenados.add(new Productos("REP39481", "Repuestos - Cadena", "CJR1837", 15.0));
     }
 }
